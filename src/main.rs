feca022758cfcec4694ff09535a7f2d0c27b7657
kamilsamaj <kamilsamaj@gmail.com>
2022-02-06 16:07:13 +0100

    empty gtk4 window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow};
+
 fn main() {
-    println!("Hello, world!");
+    // Create a new application
+    let app = Application::builder()
+        .application_id("org.gtk-rs.example")
+        .build();
+
+    // Connect to "activate" signal of `app`
+    app.connect_activate(build_ui);
+
+    // Run the application
+    app.run();
+}
+
+fn build_ui(app: &Application) {
+    // Create a window and set the title
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .build();
+
+    // Present window
+    window.present();
 }
